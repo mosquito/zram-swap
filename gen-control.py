6e245a231eb68e233979d6eb8d56ef9e519a133f
Dmitry Orlov <me@mosquito.su>
2022-03-22 12:47:37 +0300

    add docs

--- gen-control.py
+++ gen-control.py
@@ -13,7 +13,17 @@ Architecture: all
 Essential: no
 Installed-Size: $size
 Maintainer: Dmitry Orlov <me@mosquito.su>
-Description: Easy way to configure swap over zram
+Description:
+  Easy way to configure swap over zram.
+  After installation, the default behavior is to create a `ZRAM`
+  disk of the size of half of the available memory.
+  Of course, it is worth redefining this.
+  You can pass parameters by setting a variable in the /etc/default/zram-swap file:
+    ZRAM_SWAP_OPTS="--max-size 1G --algorithm lz4hc --backing-dev /dev/sdc"
+  Or using a short version of the keys:
+    ZRAM_SWAP_OPTS="-S 1G -A lz4hc -D /dev/sdc"
+  All available options available in "zram-swap --help".
+  Project distributed under Apache License Version 2.0
 """.strip())
 
 files = (
